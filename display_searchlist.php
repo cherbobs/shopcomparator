a4092d600683b970150d32b07fc6a33b42630570
cherbobs <eva.cherbonnier974@gmail.com>
2024-01-03 15:35:01 +0100

    display_search frony ok

--- display_searchlist.php
+++ display_searchlist.php
@@ -65,19 +65,31 @@ if (isset($id_category)){
 </header>
 
 <body>
-    <div class="screenview">
-        <div class="recherche-retour">
-                <div class="arrow">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
-                    <path d="M25 7L13 19L25 31" stroke="#144B90" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-                <form name="searchbar" action="" method="get">
-                    <input type="text" class="searchbar" name="keywords" id="" placeholder="Rechercher">
-                    <input type="submit" class="submit" value="Rechercher" name="valider">
-                </form>
+    <div class="search-keywords-results">
+        <div class="search-return">
+            <div class="arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
+                <path d="M25 7L13 19L25 31" stroke="#144B90" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
             </div>
+            <form name="searchbar" action="" method="get">
+                <input type="text" class="searchbar" name="keywords" id="" placeholder="Rechercher">
+                <input type="submit" class="submit" value="Rechercher" name="valider">
+            </form>
+        </div>
 
+        <div class="keywords">
+            <div class="liste-keywords">
+                <div class="keyword">
+                    <h2>Coca Cola</h2>
+                    <div class="icon-annuler">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
+                            <path d="M10 10.5L2 2.5M10 2.5L2 10.5" stroke="black" stroke-linecap="round"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <?php if (@$afficher){ ?>
             <div id="resultats">
@@ -88,17 +100,15 @@ if (isset($id_category)){
                         </div>
 
                         <div class="description-container">
-                            <div class="description-produit">
-                                <h4><?= $resultat['name']?></h4>
-                                <p>14 magasins</p>  
-                            </div>
+                            <h4><?= $resultat['name']?></h4>
+                            <p>14 magasins</p>  
                         </div>
 
                         <div class="right-arrow">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <rect width="24" height="24" rx="12" fill="#144B90"/>
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12.3328 7.25102C12.6078 6.94231 13.0815 6.91451 13.3909 7.18891L18.7484 11.941C18.9085 12.083 19 12.2864 19 12.5C19 12.7136 18.9085 12.917 18.7484 13.059L13.3909 17.8111C13.0815 18.0855 12.6078 18.0577 12.3328 17.749C12.0578 17.4403 12.0856 16.9676 12.395 16.6932L16.2793 13.2479H5.74948C5.33555 13.2479 5 12.913 5 12.5C5 12.087 5.33555 11.7521 5.74948 11.7521H16.2793L12.395 8.30685C12.0856 8.03244 12.0578 7.55973 12.3328 7.25102Z" fill="#FEFDF8"/>
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <rect width="24" height="24" rx="12" fill="#144B90"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12.3328 7.25102C12.6078 6.94231 13.0815 6.91451 13.3909 7.18891L18.7484 11.941C18.9085 12.083 19 12.2864 19 12.5C19 12.7136 18.9085 12.917 18.7484 13.059L13.3909 17.8111C13.0815 18.0855 12.6078 18.0577 12.3328 17.749C12.0578 17.4403 12.0856 16.9676 12.395 16.6932L16.2793 13.2479H5.74948C5.33555 13.2479 5 12.913 5 12.5C5 12.087 5.33555 11.7521 5.74948 11.7521H16.2793L12.395 8.30685C12.0856 8.03244 12.0578 7.55973 12.3328 7.25102Z" fill="#FEFDF8"/>
+                            </svg>
                         </div>
                     </div>
                 <?php } ?>
